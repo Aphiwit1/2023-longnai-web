47e9c9f9734b17471e1ae0767dca8d4deade2bf3
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-10-21 21:56:37 +0700

    update: yellow text

--- src/components/AppLayout/AppLAyout.tsx
+++ src/components/AppLayout/AppLAyout.tsx
@@ -1,21 +1,20 @@
-
-
 export const AppLayout = ({ children }: any) => {
-    return (
-        <>
-            <section className=" h-screen ">
-                <nav className=" h-[70px]  border-2 border-b-gray-200 px-[50px] mx-auto mb-10 flex justify-center items-center">
-                </nav>
-                
-                <div className='h-screen p-5 mb-10'>
-                    {children}
-                </div>
+  return (
+    <>
+      <section className=" h-screen ">
+        <nav className=" h-[70px]  border-2 border-b-gray-200 px-[50px] mx-auto mb-10 flex justify-center items-center">
+          <div className="bg-orange-100 px-2 py-1 rounded text-sm">
+            
+           <span>อัพเดท: สายสีเหลือง 21/0ct/2023</span> 
+          </div>
+        </nav>
 
-                {/* <footer className="h-[70px] border-2 border-t-gray-200 px-[50px] mx-auto mb-10 flex justify-center items-center">
+        <div className="h-screen p-5 mb-10">{children}</div>
+
+        {/* <footer className="h-[70px] border-2 border-t-gray-200 px-[50px] mx-auto mb-10 flex justify-center items-center">
                         
                  </footer> */}
-            </section>
-
-        </>
-    )
-}
+      </section>
+    </>
+  );
+};
